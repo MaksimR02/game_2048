0c281d1f38e6afc572e2fd94fea98d73e86bf34c
Reshetniak Maksim <resetnak32@gmail.com>
2023-05-29 17:33:10 +0400

    Added moving tiles up and stacking them

--- cell.js
+++ cell.js
@@ -6,11 +6,44 @@ export class Cell {
     this.x = x;
     this.y = y;
   }
+
   linkTile(tile) {
     tile.setXY(this.x, this.y);
     this.linkedTile = tile;
   }
-  isEmpty(){
+
+  unLinkTile() {
+    this.linkedTile = null;
+  }
+
+  isEmpty() {
     return !this.linkedTile;
+  }
+
+  linkTileForMerge(tile) {
+    tile.setXY(this.x, this.y);
+    this.linkedTileForMerge = tile;
+  }
+
+  unLinkTileForMerge() {
+    this.linkedTileForMerge = null;
+  }
+
+  hasTileForMerge() {
+    return !!this.linkedTileForMerge;
+  }
+
+  canAccept(newTile) {
+    return (
+      this.isEmpty() ||
+      (!this.hasTileForMerge() && this.linkedTile.value === newTile.value)
+    );
+  }
+  mergeTiles() {
+    this.linkedTile.setValue(
+      this.linkedTile.value + this.linkedTileForMerge.value
+    );
+    this.linkedTileForMerge.removeFromDOM();
+    this.unLinkTileForMerge();
   }
 }
