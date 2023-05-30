2a756df6dddec4555bfb1ca4227523fd3ed15aa4
Reshetniak Maksim <resetnak32@gmail.com>
2023-05-30 14:24:57 +0400

    Added the appearance of a new tile after movement, fixed the animation of the appearance of the tile

--- tile.js
+++ tile.js
@@ -24,4 +24,9 @@ export class Tile {
   removeFromDOM() {
     this.tileElement.remove();
   }
+  waitForTransitionEnd(){
+    return new Promise(resolve=>{
+      this.tileElement.addEventListener("transitionend",resolve,{once:true});
+    });
+  }
 }
